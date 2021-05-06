ae2aea229dcb65cf601a5a09b7e22066b104a049
shanart <shanin.x@gmail.com>
2021-05-06 21:50:37 +0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'radioTrainer';
+
+    symbols: Array<string> = [
+        "A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",
+        "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "+"
+    ]
+
+    
+
+    private PWMToSeconds(value: number): number {
+        return value / 12;
+    }
+
+    private SecondsToPWM(value: number): number {
+        return value * 12;
+    }
 }
