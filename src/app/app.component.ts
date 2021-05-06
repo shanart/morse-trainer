7847ca26b9feb541e1b64ed9adc88b9d807a5b52
shanart <shanin.x@gmail.com>
2021-05-06 23:55:08 +0300

    making counter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { BehaviorSubject } from 'rxjs'; 
+
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
+
+    // milisecond
+    interval_speed: number = 1;
+    running: boolean = false;
+    interval_id;
+    currentCharacter: BehaviorSubject<string> = new BehaviorSubject<string>("0");
+    @ViewChild('word_per_minute', {static: true}) wpm: ElementRef;
 
     symbols: Array<string> = [
-        "A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",
+        "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
         "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "+"
     ]
 
-    
+    getIntervalSpeed() {
+        return this.interval_speed;
+    }
+
+    startCounter() {
+        this.stopCounter();
+        let i = 0;
+        const self = this;
+        this.running = true;
+        this.interval_id = setInterval(function () {
+            self.currentCharacter.next(self.getRandomCharacter());
+            i++;
+        }, this.interval_speed * 1000);
+    }
+
+    stopCounter() {
+        this.currentCharacter.next("0");
+        this.running = false;
+        clearInterval(this.interval_id);
+    }
+
+    getRandomCharacter() {
+        const randomIndex = Math.floor(Math.random() * this.symbols.length);
+        return this.symbols[randomIndex];
+    }
+
+    ngOnDestroy() {}
+    ngOnInit() {}
 
     private PWMToSeconds(value: number): number {
         return value / 12;
